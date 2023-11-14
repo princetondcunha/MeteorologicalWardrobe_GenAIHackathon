6c07adacf569d9329d069e73e4a618968ec02ffa
princetondcunha <103622011+princetondcunha@users.noreply.github.com>
2023-11-14 10:22:12 -0400

    Re-configured the prompt for image generation

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import processinput
 
 date = "November 17"
 time = "12 AM"
-activity = "Stargazing"
-location = "Halifax"
+activity = "Work"
+location = "Kundapur"
 
 coords = processinput.getcoords(location)
 
+print(coords)
+
 recommendation = gptrecommendation.getrecommendation(getweatherdata.getweather_next5days(coords['lat'],coords['lon']),date,time,activity)
 print("Recommendation: ", recommendation['outfit_recommendation'])
-#print("Recommendation Illustration: ", imagegeneration.generateimage(recommendation))
+print("Recommendation Illustration: ", imagegeneration.generateimage(recommendation,location))
